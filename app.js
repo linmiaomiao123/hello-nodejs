d8cc797d9ff01eefedf13cc98de08201061d79ad
林颖 <linying@i-counting.cn>
2019-09-13 00:45:43 +0800

    [feat] 读写文件fs related to issue #1

--- app.js
+++ app.js
@@ -14,27 +14,41 @@
 // console.log(stuff.pi);
 
 //事件
-var events = require('events');
-var util = require('util');//工具库
-var Person = function(name) {
-  this.name = name
-}
-util.inherits(Person, events.EventEmitter);
-var xiaoming = new Person('xiaoming');
-var lili = new Person('lili');
-var lucy = new Person('lucy');
-var person = [xiaoming, lili, lucy];
-console.log(person)
-person.forEach(function(person) {
-  person.on('speak', function(message) {
-    console.log(person.name + ' said: ' + message);
-  })
-})
-xiaoming.emit('speak', 'hi');
-lucy.emit('speak', 'I want a curry');
+// var events = require('events');
+// var util = require('util');//工具库
+// var Person = function(name) {
+//   this.name = name
+// }
+// util.inherits(Person, events.EventEmitter);
+// var xiaoming = new Person('xiaoming');
+// var lili = new Person('lili');
+// var lucy = new Person('lucy');
+// var person = [xiaoming, lili, lucy];
+// console.log(person)
+// person.forEach(function(person) {
+//   person.on('speak', function(message) {
+//     console.log(person.name + ' said: ' + message);
+//   })
+// })
+// xiaoming.emit('speak', 'hi');
+// lucy.emit('speak', 'I want a curry');
 
 // var myEmitter = new events.EventEmitter();
 // myEmitter.on('someEvent', function(message){
 //   console.log(message);
 // })
 // myEmitter.emit('someEvent', ' the event was emitted');
+
+// 读写文件（同步异步）
+var fs = require('fs');
+// var readMe = fs.readFileSync("readMe.txt", "utf8");//同步
+var readMe = fs.readFile("readMe.txt", "utf8", function(err, data) {
+  console.log(data);//异步
+  fs.writeFile('writeMe.txt', data, function() {
+    console.log('writeMe has finished');
+  })
+});
+// var waitTill = new Date(new Date().getTime() + 4 * 1000);
+// while(waitTill > new Date()){} //等待4s
+console.log('finished');
+// fs.writeFileSync('writeMe.txt', 'hello world');
