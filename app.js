bc63c5942a9dd5819e9431ca76bd1e6294b4c470
林颖 <linying@i-counting.cn>
2019-09-13 00:59:01 +0800

    [feat] 创建和删除目录 related to issue #1

--- app.js
+++ app.js
@@ -1,54 +1,17 @@
-// 回调函数
-// function callFunction(fun, name) {
-//   fun(name);
-// }
-
-// callFunction(function(name) {
-//   console.log(name + " Bye")
-// }, "rails365")
-
-// 模块 （结合counter.js一起使用）
-// var stuff = require('./count')
-// console.log(stuff.counter(['ruby', 'nodejs', 'react']));
-// console.log(stuff.adder(3, 2));
-// console.log(stuff.pi);
-
-//事件
-// var events = require('events');
-// var util = require('util');//工具库
-// var Person = function(name) {
-//   this.name = name
-// }
-// util.inherits(Person, events.EventEmitter);
-// var xiaoming = new Person('xiaoming');
-// var lili = new Person('lili');
-// var lucy = new Person('lucy');
-// var person = [xiaoming, lili, lucy];
-// console.log(person)
-// person.forEach(function(person) {
-//   person.on('speak', function(message) {
-//     console.log(person.name + ' said: ' + message);
-//   })
-// })
-// xiaoming.emit('speak', 'hi');
-// lucy.emit('speak', 'I want a curry');
-
-// var myEmitter = new events.EventEmitter();
-// myEmitter.on('someEvent', function(message){
-//   console.log(message);
-// })
-// myEmitter.emit('someEvent', ' the event was emitted');
-
-// 读写文件（同步异步）
 var fs = require('fs');
-// var readMe = fs.readFileSync("readMe.txt", "utf8");//同步
-var readMe = fs.readFile("readMe.txt", "utf8", function(err, data) {
-  console.log(data);//异步
-  fs.writeFile('writeMe.txt', data, function() {
-    console.log('writeMe has finished');
+// fs.unlinkSync('writeMe.txt')//同步
+// fs.writeFileSync('writeMe.txt', 'Hello word')
+// fs.unlink('writeMe.txt', function() {
+//   console.log('delete writeMe.txt file')
+// })
+// fs.mkdirSync('stuff')//新建目录
+// fs.rmdirSync('stuff')//删除目录
+
+//潜逃很深的话， 容易引起地狱回调
+fs.mkdir('stuff', function() {
+  fs.readFile('readMe.txt', 'utf8', function(err, data) {
+    fs.writeFile('./stuff/writeMe.txt', data, function() {
+      console.log('copy successfully')
+    })
   })
-});
-// var waitTill = new Date(new Date().getTime() + 4 * 1000);
-// while(waitTill > new Date()){} //等待4s
-console.log('finished');
-// fs.writeFileSync('writeMe.txt', 'hello world');
+})
