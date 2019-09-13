e071d03a92c6e0e315df2a7a11aa46b1eba6012f
林颖 <linying@i-counting.cn>
2019-09-13 10:39:56 +0800

    [feat] 服务器 http.createServer response.writeHead response.end server.listen related to issue #1

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-var fs = require('fs');
-var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');//创建输入流（事件的实例）
-var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt', 'utf8')
-// myReadStream.setEncoding('utf8')
-// var data = ""
-// myReadStream.on('data', function(chunk) {
-//   // data += chunk;
-//   myWriteStream.write(chunk);
-// })
-// myReadStream.on('end', function() {
-//   console.log(data)
+//web服务器
+var http = require('http');
+//创建一个服务器赋值给server变量
+// var server = http.createServer(function(request, response) {
+//   console.log('Request received');
+//   response.writeHead(200, {'Content-Type': 'text/plain'});//传了个文本给浏览器
+//   // response.write('hello from out pplication');
+//   response.end('hello from out plication');
 // })
 
-// var writeData = 'hello world';
-// myWriteStream.write(writeData);
-// myWriteStream.end();
-// myWriteStream.on('finish', function() {
-//   console.log('finished')
-// })
+var onRequest = function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello from out plication');
+} 
+var server = http.createServer(onRequest);
 
-//管道
-myReadStream.pipe(myWriteStream);
+//监听端口
+server.listen(3000, '127.0.0.1');
+console.log('Server started on localhost port 3000')
