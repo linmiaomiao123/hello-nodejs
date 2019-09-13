f37bd5233038261685e4f4d083e2d1a7c3351164
林颖 <linying@i-counting.cn>
2019-09-13 10:23:19 +0800

    [feat] 流和管道 related to issue #1

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 var fs = require('fs');
-// fs.unlinkSync('writeMe.txt')//同步
-// fs.writeFileSync('writeMe.txt', 'Hello word')
-// fs.unlink('writeMe.txt', function() {
-//   console.log('delete writeMe.txt file')
+var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');//创建输入流（事件的实例）
+var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt', 'utf8')
+// myReadStream.setEncoding('utf8')
+// var data = ""
+// myReadStream.on('data', function(chunk) {
+//   // data += chunk;
+//   myWriteStream.write(chunk);
 // })
-// fs.mkdirSync('stuff')//新建目录
-// fs.rmdirSync('stuff')//删除目录
+// myReadStream.on('end', function() {
+//   console.log(data)
+// })
 
-//潜逃很深的话， 容易引起地狱回调
-fs.mkdir('stuff', function() {
-  fs.readFile('readMe.txt', 'utf8', function(err, data) {
-    fs.writeFile('./stuff/writeMe.txt', data, function() {
-      console.log('copy successfully')
-    })
-  })
-})
+// var writeData = 'hello world';
+// myWriteStream.write(writeData);
+// myWriteStream.end();
+// myWriteStream.on('finish', function() {
+//   console.log('finished')
+// })
+
+//管道
+myReadStream.pipe(myWriteStream);
