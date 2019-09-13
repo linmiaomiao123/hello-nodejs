9a0ec6827e28a51f6951fa188a54f4b03a8f9d4a
林颖 <linying@i-counting.cn>
2019-09-13 11:21:46 +0800

    [feat] 响应html related to issue #1

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //web服务器
 var http = require('http');
+var fs = require('fs');
 //创建一个服务器赋值给server变量
 // var server = http.createServer(function(request, response) {
 //   console.log('Request received');
@@ -8,16 +9,48 @@ var http = require('http');
 //   response.end('hello from out plication');
 // })
 
+//纯文本
+// var onRequest = function(request, response) {
+//   response.writeHead(200, {'Content-Type': 'text/plain'});
+//   response.end('Hello word');
+// } 
+
 //web服务器响应json
+// var onRequest = function(request, response) {
+//   response.writeHead(200, {'Content-Type': 'application/json'});
+//   var myObj = {
+//     name: 'hfpp2012',
+//     job: 'programmer',
+//     age: 27
+//   }
+//   response.end(JSON.stringify(myObj));
+// } 
+
+//web服务器响应html
 var onRequest = function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/json'});
-  var myObj = {
-    name: 'hfpp2012',
-    job: 'programmer',
-    age: 27
-  }
-  response.end(JSON.stringify(myObj));
-} 
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  // var htmlFile = '<!DOCTYPE html>' +
+  // '<html lang="en">' +
+  
+  // '<head>' +
+  //     '<meta charset="UTF-8">' +
+  //     '<meta name="viewport" content="width=device-width, initial-scale=1.0">' +
+  //     '<meta http-equiv="X-UA-Compatible" content="ie=edge">' +
+  //     '<title>Document</title>' +
+  // '</head>' +
+  
+  // '<body>' +
+  
+  // '</body>' +
+  
+  // '</html>'
+  // response.end(htmlFile);
+  
+  //使用流来读取文件， 写文件
+  var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+  myReadStream.pipe(response);
+}
+
 var server = http.createServer(onRequest);
 
 //监听端口
